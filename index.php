0eb3ba4a7a86daf19702d5549fe61b807a3af874
ALTERPRESS <52164519+alterpress@users.noreply.github.com>
2019-06-24 16:52:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-  echo '<!DOCTYPE html><html><head><title>ALTERPRESS</title><head> <style>body {font-weight: normal; font-size: 11px;
-  font-family: Arial;border:1px solid gray;height:478px;width:638px;margin:0;padding:0; } </style><pre>Please Log In</pre></body></html>'; 
+  echo '<!DOCTYPE html><html><head><title>ALTERPRESS</title></head> <style>body {font-weight: normal; font-size: 11px;
+  font-family: Arial;border:1px solid silver;height:478px;width:638px;margin:0;padding:0; } </style><pre>ALTER PRESS</pre></body></html>'; 
 ?>
