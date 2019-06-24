e8c73024f0590cc294b66a70dd42bf486a9571d4
ALTERPRESS <52164519+alterpress@users.noreply.github.com>
2019-06-24 22:27:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
   echo '<!DOCTYPE html><html><head><title>ALTERPRESS</title></head> <style>body {font-weight: normal; font-size: 11px;
-  font-family: Arial;border:1px solid silver;height:498px;width:638px;margin:0;padding:0; } </style><pre>. .. ВОЙТИ</pre></body></html>'; 
+  font-family: Arial;border:1px solid silver;height:498px;width:638px;margin:0;padding:0; } </style><pre>[]</pre></body></html>'; 
 ?>
